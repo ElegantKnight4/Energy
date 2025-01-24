a618f02b166b8dae5dbf5864d2e97ab84505fcf0
ElegantKnight4 <zourlapanos@gmail.com>
2025-01-24 10:06:18 +0200

    Create Energy.java

---
+++ Energy.java
@@ -0,0 +1,6 @@
+//Setting Titan = 1 we have E = Titan*mcmod2
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("E = mcmod2 = lne*mcmod2 = gamma*lne*mcmod2 = Titan*mcmod2");
+    }
+}
